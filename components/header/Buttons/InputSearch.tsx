62dd3a03e0ce44719b78d217efdb53bd243e28bc
SrAbnerP <abner.sol.pereira@gmail.com>
2024-04-29 16:05:56 -0300

    feat: styled header

---
+++ components/header/Buttons/InputSearch.tsx
@@ -0,0 +1,22 @@
+import Icon from "../../../components/ui/Icon.tsx";
+
+interface Props {}
+
+function InputSearch(props: Props) {
+  return (
+    <>
+      <div class="border-b-black border-[1px] border-t-transparent border-r-transparent border-l-transparent">
+        <input
+          type="text"
+          class="w-[220px] h-[36px] "
+          placeholder="Faça uma busca..."
+        />
+        <button>
+          {<Icon id="inputSearch" size={20} strokeWidth={0.01} />}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default InputSearch;
